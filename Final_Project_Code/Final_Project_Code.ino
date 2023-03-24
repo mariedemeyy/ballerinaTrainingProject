1bbff7d2d902f0fd458f5df3f36b63e425229003
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-03-24 12:04:47 -0400

    Start to implement digital pin readings

--- Final_Project_Code/Final_Project_Code.ino
+++ Final_Project_Code/Final_Project_Code.ino
@@ -4,24 +4,39 @@
     SparkFun Electronics
     created on 10/29/11
     */
-    int ledPin = 13;  // LED is attached to digital pin 13
+    // int ledPin = 13;  // LED is attached to digital pin 13
 
-    hello
-
-    // Force sensor code
-    int forcePin0 = A0; // Analog Pin 0 
-    int interruptDriverPin = 8; // Pin to 
-    int x = 0;  // variable to be updated by the interrupt
+    // DEBUG ANALOG FORCE READINGS (REMOVE WIRING AND CODE TO THESE LATER)
+    int forceDebugPin0 = A0; // Analog Pin 0 that we will use to debug our circuit 
+    int forceDebugPin1 = A1; // Analog Pin 1 that we will use to debug our circuit 
+    int forceDebugPin2 = A2; // Analog Pin 2 that we will use to debug our circuit 
+    int forceDebugPin3 = A3; // Analog Pin 3 that we will use to debug our circuit 
     
-    void setup() {                
-      //enable interrupt 0 (pin 2) which is connected to a button
-      //jump to the increment function on falling edge
-      pinMode(ledPin, OUTPUT);
-      pinMode(interruptDriverPin, OUTPUT);
-      attachInterrupt(digitalPinToInterrupt(2), fallingEdge, FALLING);
+    // DEBUG INTERRUPT COUNT 
+    int debugInterruptCounter = 0;  // variable to be updated by the interrupt
+  
+    // int interruptDriverPin = 8; // Pin to 
+
+    // DIGITAL FORCE READING PINS (THIS WILL CONNECT FROM THE "Y" IN OUR QUAD 2-INPUT AND)
+    int forcePin0 = 9; // Digital Pin 9
+    int forcePin1 = 10; // Digital Pin 10
+    int forcePin2 = 11; // Digital Pin 11
+    int forcePin3 = 12; // Digital Pin 12
+
+    void setup() {   
+      pinMode(forcePin0, INPUT);             
+      // pinMode(ledPin, OUTPUT);
+
+      // BASICALLY 4 FORCE SENSOR READINGS... but only 1 interrupt handler should be used.
+      // How do we go from 4 digital pins --> 1. 
+      // ex: 1 0 1 1 = 1
+      // ex: 0 0 0 1 = 1
+      attachInterrupt(digitalPinToInterrupt(2), fallingEdge, FALLING); // 1 to 0 
       Serial.begin(9600);  //turn on serial communication
+      
+      // DEBUG SERIAL STATEMENTS
       Serial.println("");
-      Serial.println(x, DEC);
+      Serial.println(debugInterruptCounter, DEC);
     }
     
     void loop() {
@@ -44,11 +59,11 @@
       }
       delay(500);
       Serial.print("; increment number: "); // Debug statement
-      Serial.println(x, DEC); //print x to serial monitor
+      Serial.println(debugInterruptCounter, DEC); //print x to serial monitor
     }
 
     // Interrupt service routine for interrupt 0
     void fallingEdge() {
-        x++;
+        debugInterruptCounter++;
         digitalWrite(ledPin, HIGH);
     }
