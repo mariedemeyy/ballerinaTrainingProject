363eb299e349147c9141f5547a0bd594d94ca757
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-03-30 16:05:17 -0400

    Delete old code from first lab session

--- Final_Project_Code/Final_Project_Code.ino
+++ Final_Project_Code/Final_Project_Code.ino
@@ -1,8 +1,9 @@
-// Include libraries
+////////////////////////////////////////////
+// LCD VARIABLES
+///////////////////////////////////////////
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-// LCD VARIABLES
 // define some values used by the panel and buttons
 int lcd_key     = 0;
 int adc_key_in  = 0;
@@ -14,9 +15,6 @@ int adc_key_in  = 0;
 #define btnNONE   5
 bool repCountSelected = false;
 bool startButtonSelected = false;
-
-// GENERAL EXERCISE VARIABLES
-int selectedRepCount;
 
 // read the buttons
 int read_LCD_buttons()
@@ -33,43 +31,60 @@ int read_LCD_buttons()
  return btnNONE;  // when all others fail, return this...
 }
 
-    // DEBUG ANALOG FORCE READINGS (REMOVE WIRING AND CODE TO THESE LATER)
-    // int forceDebugPin0 = A0; // Analog Pin 0 that we will use to debug our circuit 
-    // int forceDebugPin1 = A1; // Analog Pin 1 that we will use to debug our circuit 
-    // int forceDebugPin2 = A2; // Analog Pin 2 that we will use to debug our circuit 
-    // int forceDebugPin3 = A3; // Analog Pin 3 that we will use to debug our circuit 
+////////////////////////////////////////////
+// DEBUG VARIABLES (REMOVE LATER)
+///////////////////////////////////////////
 
-    // Digital force sensor reading, this is the result of an AND gate with A = 1, B = force sensor reading
-    int heelSensor = 11; // interrupt event
-    int ballSensor = 12;
-    int lowerBoxSensor = 10; // interrupt event
-    int higherBoxSensor = 13; 
+// int forceDebugPin0 = A0; // Analog Pin 0 that we will use to debug our circuit 
+// int forceDebugPin1 = A1; // Analog Pin 1 that we will use to debug our circuit 
+// int forceDebugPin2 = A2; // Analog Pin 2 that we will use to debug our circuit 
+// int forceDebugPin3 = A3; // Analog Pin 3 that we will use to debug our circuit 
 
-    volatile int heelSensorVal = 0;
-    volatile int ballSensorVal = 0;
-    volatile int lowerBoxSensorVal = 0;
-    volatile int higherBoxSensorVal = 0;
+////////////////////////////////////////////
+// EXERCISE VARIABLES
+///////////////////////////////////////////
+
+int selectedRepCount;
+
+int heelSensor = 11; // interruptable event
+int ballSensor = 12;
+int lowerBoxSensor = 10; // interruptable event
+int higherBoxSensor = 13; 
+
+// These variables are modified in the ISR, declare as volatile to make sure they are updated correctly
+volatile int heelSensorVal = 0;
+volatile int ballSensorVal = 0;
+volatile int lowerBoxSensorVal = 0;
+volatile int higherBoxSensorVal = 0;
+
+////////////////////////////////////////////
+// ISR VARIABLES
+///////////////////////////////////////////
+int interruptPin = 2;
+int statusInterrupt = 0;
 
     void setup() {
-      Serial.begin(9600);  //turn on serial communication
+      Serial.begin(9600);
 
       pinMode(heelSensor, INPUT); 
       pinMode(ballSensor, INPUT);
       pinMode(lowerBoxSensor, INPUT);
       pinMode(higherBoxSensor, INPUT);
+
+      // LCD display code to guide user through their exercises
       lcd.begin(16,2);
       lcd.setCursor(0,0);
       lcd.print("Place foot");
       lcd.setCursor(0,1);
       lcd.print("on board!");
-      delay(1000); // change longer for demo
+      delay(1000); // DEMO: change longer for demo
       lcd.clear();
 
       lcd.setCursor(0,0);
       lcd.print("How many ");
       lcd.setCursor(0,1);
       lcd.print("reps?");
-      delay(1000); // change longer for demo
+      delay(1000); // DEMO: change longer for demo
       lcd.clear();
 
       lcd.setCursor(0,0);
@@ -105,7 +120,7 @@ int read_LCD_buttons()
         } 
       }
 
-      delay(1000);
+      delay(1000); // DEMO
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("Hit up to");
@@ -128,50 +143,27 @@ int read_LCD_buttons()
         }
       }
 
-      // At the end of the setup loop, start the interrupt (not before though because we don't want to randomly trigger ISR)
-      attachInterrupt(digitalPinToInterrupt(2), readFootPosition, CHANGE); // will sense changes from 1 to 0 OR 0 to 1 (required for our application)      
+      // At the end of the setup loop, attach the interrupt (not before though because we don't want to randomly trigger ISR)
+      attachInterrupt(interruptPin, readFootPosition, CHANGE); // will sense changes from 1 to 0 OR 0 to 1 (required for our application)      
     }
     
     void loop() {
-      // Write LED to low 
-      // int blue0Val = digitalRead(blue0); // BOX SENSOR
-      // int yellow1Val = digitalRead(yellow1);
-      // int green2Val = digitalRead(green2);
-      // int white3Val = digitalRead(white3); //  HEEL SENSOR
-
-      // Read the analog value
       Serial.print("force reading (heelSensor): "); // Debug statement
       Serial.println(heelSensorVal); 
-      // Serial.print("force reading (yellow1): "); // Debug statement
-      // Serial.println(yellow1Val); 
-      // Serial.print("force reading (green2): "); // Debug statement
-      // Serial.println(green2Val); 
       Serial.print("force reading (lowerBoxSensor): "); // Debug statement
       Serial.println(lowerBoxSensorVal); 
-      Serial.print("interrupt pin (2): "); // Debug statement
-      int interruptPin = 2;
-      int statusInterrupt = digitalRead(interruptPin);
+      statusInterrupt = digitalRead(interruptPin);
+      Serial.print("Interrupt pin (2): "); // Debug statement
       Serial.println(statusInterrupt); 
 
-//       // From analog value reading, write digital value (mapping is subject to change) 
-//       if(forceReading0 > 0) { 
-// //        Serial.println("Force reading greater than 0"); 
-//         digitalWrite(interruptDriverPin, HIGH);
-//       }
-//       else {
-// //        Serial.println("Force reading less than or equal to 0"); 
-//         digitalWrite(interruptDriverPin, LOW);
-//       }
-      // Serial.print("; increment number: "); // Debug statement
-      // Serial.println(debugInterruptCounter, DEC); //print x to serial monitor
+      
       delay(500);
     }
 
-    // Interrupt service routine for interrupt 0
     void readFootPosition() {
       // Get a quick reading of all of the foot sensors to get an understanding of the state
-      heelSensorVal = digitalRead(heelSensor); // BOX SENSOR
-      // yellow1Val = digitalRead(yellow1);
-      // green2Val = digitalRead(green2);
-      lowerBoxSensorVal = digitalRead(lowerBoxSensor); //  HEEL SENSOR
+      heelSensorVal = digitalRead(heelSensor);
+      ballSensorVal = digitalRead(ballSensor);
+      lowerBoxSensorVal = digitalRead(lowerBoxSensor);
+      higherBoxSensorVal = digitalRead(higherBoxSensor);
     }
